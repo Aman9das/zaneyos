04e2b98b16777a70dee9a5007d453d1f89d8d1e9
Tyler Kelley <tylerzanekelley@gmail.com>
2024-01-14 01:58:15 -0600

    Add Hashes That Are Necessary

--- config/kitty.nix
+++ config/kitty.nix
@@ -15,41 +15,41 @@
       background_opacity = "0.85";
     };
     extraConfig = ''
-      foreground ${config.colorScheme.colors.base0F}
-      background ${config.colorScheme.colors.base00}
-      color0  ${config.colorScheme.colors.base00}
-      color8  ${config.colorScheme.colors.base00}
-      color1  ${config.colorScheme.colors.base01}
-      color9  ${config.colorScheme.colors.base09}
-      color2  ${config.colorScheme.colors.base02}
-      color10 ${config.colorScheme.colors.base0A}
-      color3  ${config.colorScheme.colors.base03}
-      color11 ${config.colorScheme.colors.base0B}
-      color4  ${config.colorScheme.colors.base04}
-      color12 ${config.colorScheme.colors.base0C}
-      color5  ${config.colorScheme.colors.base05}
-      color13 ${config.colorScheme.colors.base0D}
-      color6  ${config.colorScheme.colors.base06}
-      color14 ${config.colorScheme.colors.base0E}
-      color7  ${config.colorScheme.colors.base07}
-      color15 ${config.colorScheme.colors.base0F}
-      cursor  ${config.colorScheme.colors.base07}
-      cursor_text_color ${config.colorScheme.colors.base00}
+      foreground #${config.colorScheme.colors.base0F}
+      background #${config.colorScheme.colors.base00}
+      color0  #${config.colorScheme.colors.base00}
+      color8  #${config.colorScheme.colors.base00}
+      color1  #${config.colorScheme.colors.base01}
+      color9  #${config.colorScheme.colors.base09}
+      color2  #${config.colorScheme.colors.base02}
+      color10 #${config.colorScheme.colors.base0A}
+      color3  #${config.colorScheme.colors.base03}
+      color11 #${config.colorScheme.colors.base0B}
+      color4  #${config.colorScheme.colors.base04}
+      color12 #${config.colorScheme.colors.base0C}
+      color5  #${config.colorScheme.colors.base05}
+      color13 #${config.colorScheme.colors.base0D}
+      color6  #${config.colorScheme.colors.base06}
+      color14 #${config.colorScheme.colors.base0E}
+      color7  #${config.colorScheme.colors.base07}
+      color15 #${config.colorScheme.colors.base0F}
+      cursor  #${config.colorScheme.colors.base07}
+      cursor_text_color #${config.colorScheme.colors.base00}
       selection_foreground none
-      selection_background ${config.colorScheme.colors.base08}
-      url_color ${config.colorScheme.colors.base02}
-      active_border_color ${config.colorScheme.colors.base04}
-      inactive_border_color ${config.colorScheme.colors.base00}
-      bell_border_color ${config.colorScheme.colors.base03}
+      selection_background #${config.colorScheme.colors.base08}
+      url_color #${config.colorScheme.colors.base02}
+      active_border_color #${config.colorScheme.colors.base04}
+      inactive_border_color #${config.colorScheme.colors.base00}
+      bell_border_color #${config.colorScheme.colors.base03}
       tab_bar_style fade
       tab_fade 1
-      active_tab_foreground   ${config.colorScheme.colors.base04}
-      active_tab_background   ${config.colorScheme.colors.base00}
+      active_tab_foreground   #${config.colorScheme.colors.base04}
+      active_tab_background   #${config.colorScheme.colors.base00}
       active_tab_font_style   bold
-      inactive_tab_foreground ${config.colorScheme.colors.base07}
-      inactive_tab_background ${config.colorScheme.colors.base08}
+      inactive_tab_foreground #${config.colorScheme.colors.base07}
+      inactive_tab_background #${config.colorScheme.colors.base08}
       inactive_tab_font_style bold
-      tab_bar_background ${config.colorScheme.colors.base00}
+      tab_bar_background #${config.colorScheme.colors.base00}
     '';
   };
 }
--- config/waybar.nix
+++ config/waybar.nix
@@ -114,115 +114,115 @@
 		    background-color: rgba(26,27,38,0);
     		border-bottom: 1px solid rgba(26,27,38,0);
     		border-radius: 0px;
-		    color: ${config.colorScheme.colors.base0F};
+		    color: #${config.colorScheme.colors.base0F};
 	}
 	#workspaces {
-		    background: linear-gradient(180deg, ${config.colorScheme.colors.base00}, ${config.colorScheme.colors.base08});
+		    background: linear-gradient(180deg, #${config.colorScheme.colors.base00}, #${config.colorScheme.colors.base08});
     		margin: 5px;
     		padding: 0px 1px;
     		border-radius: 15px;
     		border: 0px;
     		font-style: normal;
-    		color: ${config.colorScheme.colors.base00};
+    		color: #${config.colorScheme.colors.base00};
 	}
 	#workspaces button {
     		padding: 0px 5px;
     		margin: 4px 3px;
     		border-radius: 15px;
     		border: 0px;
-    		color: ${config.colorScheme.colors.base00};
-    		background-color: ${config.colorScheme.colors.base00};
+    		color: #${config.colorScheme.colors.base00};
+    		background-color: #${config.colorScheme.colors.base00};
     		opacity: 1.0;
     		transition: all 0.3s ease-in-out;
 	}
 	#workspaces button.active {
-    		color: ${config.colorScheme.colors.base00};
-    		background: ${config.colorScheme.colors.base04};
+    		color: #${config.colorScheme.colors.base00};
+    		background: #${config.colorScheme.colors.base04};
     		border-radius: 15px;
     		min-width: 40px;
     		transition: all 0.3s ease-in-out;
     		opacity: 1.0;
 	}
 	#workspaces button:hover {
-    		color: ${config.colorScheme.colors.base00};
-    		background: ${config.colorScheme.colors.base04};
+    		color: #${config.colorScheme.colors.base00};
+    		background: #${config.colorScheme.colors.base04};
     		border-radius: 15px;
     		opacity: 1.0;
 	}
 	tooltip {
-  		background: ${config.colorScheme.colors.base00};
-  		border: 1px solid ${config.colorScheme.colors.base04};
+  		background: #${config.colorScheme.colors.base00};
+  		border: 1px solid #${config.colorScheme.colors.base04};
   		border-radius: 10px;
 	}
 	tooltip label {
-  		color: ${config.colorScheme.colors.base07};
+  		color: #${config.colorScheme.colors.base07};
 	}
 	#window {
-    		color: ${config.colorScheme.colors.base06};
-    		background: ${config.colorScheme.colors.base00};
+    		color: #${config.colorScheme.colors.base06};
+    		background: #${config.colorScheme.colors.base00};
     		border-radius: 0px 15px 50px 0px;
     		margin: 5px 5px 5px 0px;
     		padding: 2px 20px;
 	}
 	#memory {
-    		color: ${config.colorScheme.colors.base05};
-    		background: ${config.colorScheme.colors.base00};
+    		color: #${config.colorScheme.colors.base05};
+    		background: #${config.colorScheme.colors.base00};
     		border-radius: 15px 50px 15px 50px;
     		margin: 5px;
     		padding: 2px 20px;
 	}
 	#clock {
-    		color: ${config.colorScheme.colors.base07};
-    		background: ${config.colorScheme.colors.base00};
+    		color: #${config.colorScheme.colors.base07};
+    		background: #${config.colorScheme.colors.base00};
     		border-radius: 15px 50px 15px 50px;
     		margin: 5px;
     		padding: 2px 20px;
 	}
 	#cpu {
-    		color: ${config.colorScheme.colors.base05};
-    		background: ${config.colorScheme.colors.base00};
+    		color: #${config.colorScheme.colors.base05};
+    		background: #${config.colorScheme.colors.base00};
     		border-radius: 50px 15px 50px 15px;
     		margin: 5px;
     		padding: 2px 20px;
 	}
 	#disk {
-    		color: ${config.colorScheme.colors.base02};
-    		background: ${config.colorScheme.colors.base00};
+    		color: #${config.colorScheme.colors.base02};
+    		background: #${config.colorScheme.colors.base00};
     		border-radius: 15px 50px 15px 50px;
     		margin: 5px;
     		padding: 2px 20px;
 	}
 	#battery {
-    		color: ${config.colorScheme.colors.base01};
-    		background: ${config.colorScheme.colors.base00};
+    		color: #${config.colorScheme.colors.base01};
+    		background: #${config.colorScheme.colors.base00};
     		border-radius: 15px;
     		margin: 5px;
     		padding: 2px 20px;
 	}
 	#network {
-    		color: ${config.colorScheme.colors.base03};
-    		background: ${config.colorScheme.colors.base00};
+    		color: #${config.colorScheme.colors.base03};
+    		background: #${config.colorScheme.colors.base00};
     		border-radius: 50px 15px 50px 15px;
     		margin: 5px;
     		padding: 2px 20px;
 	}
 	#tray {
-    		color: ${config.colorScheme.colors.base07};
-    		background: ${config.colorScheme.colors.base00};
+    		color: #${config.colorScheme.colors.base07};
+    		background: #${config.colorScheme.colors.base00};
     		border-radius: 15px 0px 0px 50px;
     		margin: 5px 0px 5px 5px;
     		padding: 2px 20px;
 	}
 	#pulseaudio {
-    		color: ${config.colorScheme.colors.base05};
-    		background: ${config.colorScheme.colors.base00};
+    		color: #${config.colorScheme.colors.base05};
+    		background: #${config.colorScheme.colors.base00};
     		border-radius: 50px 15px 50px 15px;
     		margin: 5px;
     		padding: 2px 20px;
 	}
 	#custom-notification {
-    		color: ${config.colorScheme.colors.base04};
-    		background: ${config.colorScheme.colors.base00};
+    		color: #${config.colorScheme.colors.base04};
+    		background: #${config.colorScheme.colors.base00};
     		border-radius: 15px 50px 15px 50px;
     		margin: 5px;
     		padding: 2px 20px;
