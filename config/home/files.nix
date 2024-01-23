83b535e171be22b4a954e64c1bad4194ccb3720c
Tyler Kelley <tylerzanekelley@gmail.com>
2024-01-22 23:46:45 -0600

    Changing to and testing new sddm theme derivation

--- config/home/files.nix
+++ config/home/files.nix
@@ -5,7 +5,8 @@
   home.file.".config/zaney-stinger.mov".source = ./files/media/zaney-stinger.mov;
   home.file.".emoji".source = ./files/emoji;
   home.file.".base16-themes".source = ./files/base16-themes;
-  home.file.".face".source = ./files/face.jpg;
+  home.file.".face".source = ./files/face.jpg; # For GDM
+  home.file.".face.icon".source = ./files/face.jpg; # For SDDM
   home.file.".config/rofi/rofi.jpg".source = ./files/rofi.jpg;
   home.file.".config/starship.toml".source = ./files/starship.toml;
   home.file.".local/share/fonts" = {
--- config/system/displaymanager.nix
+++ config/system/displaymanager.nix
@@ -10,11 +10,17 @@
       enable = true;
       autoNumlock = true;
       wayland.enable = true;
-      theme = "sugar-dark";
+      theme = "tokyo-night-sddm";
     };
   };
 
-  environment.systemPackages = let themes = pkgs.callPackage ../pkgs/sddm-sugar-dark.nix {}; in [ 
-    themes.sddm-sugar-dark 
+  environment.systemPackages =
+let
+    sugar = pkgs.callPackage ../pkgs/sddm-sugar-dark.nix {};
+    tokyo-night = pkgs.libsForQt5.callPackage ../pkgs/sddm-tokyo-night.nix {};
+in [ 
+    sugar.sddm-sugar-dark # Name: sugar-dark
+    tokyo-night # Name: tokyo-night-sddm
+    pkgs.libsForQt5.qt5.qtgraphicaleffects
   ];
 }
