11ecebec54e32a13d2e75e9884ebdeef453a1aad
Tyler Kelley <tylerzanekelley@gmail.com>
2024-01-29 16:30:30 -0600

    add pam service for swaylock to fix not being able to login

--- config/system/services.nix
+++ config/system/services.nix
@@ -30,4 +30,9 @@
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
   services.blueman.enable = true;
+  security.pam.services.swaylock = {
+    text = ''
+      auth include login
+    '';
+  };
 }
