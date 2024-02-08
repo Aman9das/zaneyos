03897fd478153f4a61995c74910de8eb11267038
Jerry Starke <42114389+JerrySM64@users.noreply.github.com>
2024-02-08 06:15:45 +0100

    Add dependencies to make network scanning work

--- config/system/printer.nix
+++ config/system/printer.nix
@@ -1,4 +1,4 @@
-{ config, lib, username, ... }:
+{ config, lib, pkgs, username, ... }:
 
 let inherit (import ../../options.nix) printer; in
 lib.mkIf (printer == true) {
@@ -9,7 +9,12 @@ lib.mkIf (printer == true) {
       nssmdns4 = true;
       openFirewall = true;
     };
+    ipp-usb.enable = true;
   };
-  hardware.sane.enable = true;
+  hardware.sane = {
+    enable = true;
+    extraBackends = [pkgs.sane-airscan];
+    disabledDefaultBackends = ["escl"];
+  };
   users.users.${username}.extraGroups = ["scanner" "lp"];
 }
