3537bd5c0e724a9051989e8ccfb0b5dfd6e93285
Tyler Kelley <tylerzanekelley@gmail.com>
2024-02-20 03:53:47 -0600

    Added terminal to neovim and many more nicities

--- config/home/neovim.nix
+++ config/home/neovim.nix
@@ -48,6 +48,8 @@ in {
     
     plugins = {
       barbecue.enable = true;
+      gitsigns.enable = true;
+      toggleterm.enable = true;
       telescope = {
 	enable = true;
 	keymaps = {
@@ -62,31 +64,6 @@ in {
       comment-nvim.enable = true;
       lualine = {
         enable = true;
-        componentSeparators = {
-          left = "|";
-          right = "|";
-        };
-        sectionSeparators = {
-          left = "";
-          right = "";
-        };
-        inactiveSections = {
-          lualine_a = [ "filename" ];
-          lualine_b = null;
-          lualine_c = null;
-          lualine_x = null;
-          lualine_y = null;
-          lualine_z = [ "location" ];
-        };
-        sections = {
-          lualine_a = ["mode" "separator = { left = '' }" "right_padding = 2" ];
-          lualine_b = [ "filename" "branch" ];
-          lualine_c = [ "fileformat" ];
-          lualine_x = [ ];
-          lualine_y = [ "filetype" "progress" ];
-          lualine_z = [ "location" "separator = { right = '' }" "left_padding = 2" ];
-        };
-
       };
       startup = { 
 	enable = true;
@@ -158,11 +135,71 @@ in {
     extraConfigLua = '' 
       vim.opt.guifont = "JetBrainsMono\\ NFM,Noto_Color_Emoji:h14"
       vim.g.neovide_cursor_animation_length = 0.05
+
+      local colors = {
+        blue   = '#${theme.base0D}',
+        cyan   = '#${theme.base0C}',
+        black  = '#${theme.base00}',
+        white  = '#${theme.base05}',
+        red    = '#${theme.base08}',
+        violet = '#${theme.base0E}',
+        grey   = '#${theme.base02}',
+      }
+
+      local bubbles_theme = {
+        normal = {
+          a = { fg = colors.black, bg = colors.violet },
+          b = { fg = colors.white, bg = colors.grey },
+          c = { fg = colors.black, bg = colors.black },
+        },
+
+        insert = { a = { fg = colors.black, bg = colors.blue } },
+        visual = { a = { fg = colors.black, bg = colors.cyan } },
+        replace = { a = { fg = colors.black, bg = colors.red } },
+
+        inactive = {
+          a = { fg = colors.white, bg = colors.black },
+          b = { fg = colors.white, bg = colors.black },
+          c = { fg = colors.black, bg = colors.black },
+        },
+      }
+
+      require('lualine').setup {
+        options = {
+          theme = bubbles_theme,
+          component_separators = '|',
+          section_separators = { left = '', right = '' },
+        },
+        sections = {
+          lualine_a = {
+            { 'mode', separator = { left = '' }, right_padding = 2 },
+          },
+          lualine_b = { 'filename', 'branch' },
+          lualine_c = { 'fileformat' },
+          lualine_x = {},
+          lualine_y = { 'filetype', 'progress' },
+          lualine_z = {
+            { 'location', separator = { right = '' }, left_padding = 2 },
+          },
+        },
+        inactive_sections = {
+          lualine_a = { 'filename' },
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = { 'location' },
+        },
+        tabline = {},
+        extensions = {},
+      }
     '';
 
     extraConfigVim = ''
       set noshowmode
       inoremap jj <ESC>
+      nnoremap <silent><c-t> <Cmd>exe v:count1 . "ToggleTerm"<CR>
+      inoremap <silent><c-t> <Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>
     '';
 
     keymaps = [
