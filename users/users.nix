cea58927ced3f5d443d7e11717a9932cabbd3e88
Tyler Kelley <tylerzanekelley@gmail.com>
2024-04-04 01:05:42 -0500

    Update users so the new example user is commented out

--- users/users.nix
+++ users/users.nix
@@ -14,15 +14,16 @@ in {
       ignoreShellProgramCheck = true;
       packages = with pkgs; [];
     };
-    "newuser" = {
-      homeMode = "755";
-      hashedPassword = "$6$YdPBODxytqUWXCYL$AHW1U9C6Qqkf6PZJI54jxFcPVm2sm/XWq3Z1qa94PFYz0FF.za9gl5WZL/z/g4nFLQ94SSEzMg5GMzMjJ6Vd7.";
-      isNormalUser = true;
-      description = "New user account";
-      extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
-      shell = pkgs.${theShell};
-      ignoreShellProgramCheck = true;
-      packages = with pkgs; [];
-    };
+    # "newuser" = {
+    #   homeMode = "755";
+    #   You can get this by running - mkpasswd -m sha-512 <password>
+    #   hashedPassword = "$6$YdPBODxytqUWXCYL$AHW1U9C6Qqkf6PZJI54jxFcPVm2sm/XWq3Z1qa94PFYz0FF.za9gl5WZL/z/g4nFLQ94SSEzMg5GMzMjJ6Vd7.";
+    #   isNormalUser = true;
+    #   description = "New user account";
+    #   extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
+    #   shell = pkgs.${theShell};
+    #   ignoreShellProgramCheck = true;
+    #   packages = with pkgs; [];
+    # };
   };
 }
